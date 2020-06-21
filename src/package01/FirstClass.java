ae13481b268604273dc3517bf24b78f774a0ed9e
Sueheyla <sueheylatas@gmail.com>
2020-06-21 21:33:23 +0200

    feature branch tan ilk commit

--- src/package01/FirstClass.java
+++ src/package01/FirstClass.java
@@ -5,6 +5,8 @@ public class FirstClass {
 	public static void main(String[] args) {
 		System.out.println("Ilk Commit from local repository");
 		System.out.println("Ikinci Commit from Local Repository");
+		
+		System.out.println("Yeni branch olusturdum feature adinda");
 	}
 
 }
